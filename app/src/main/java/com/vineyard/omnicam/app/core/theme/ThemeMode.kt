af8cad9728579e73983918b2f056aa62ecbd8096
josyvine <64621103+josyvine@users.noreply.github.com>
2026-09-09 16:22:52 +0530

    Add app/src/main/java/com/vineyard/omnicam/app/core/theme/ThemeMode.kt via FastGit Mobile App

---
+++ app/src/main/java/com/vineyard/omnicam/app/core/theme/ThemeMode.kt
@@ -0,0 +1,8 @@
+package com.vineyard.omnicam.app.core.theme
+
+enum class ThemeMode(val title: String) {
+    SYSTEM("System Default"),
+    LIGHT("Light Clean"),
+    DARK("Slate Dark"),
+    AMOLED("OLED Pitch Black")
+}
